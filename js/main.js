5c41cbc419c93c66081fb26baf297ffaab738ddf
Silvestre Hernandez <silvestre.hdz.h@gmail.com>
2024-01-03 15:42:56 -0600

    implementacion de formulario de login usando el localStorage

--- js/main.js
+++ js/main.js
@@ -81,4 +81,28 @@ $(document).ready(function(){
 
         return false;
       });
+
+      // Login falso
+
+      $("#login form").submit(function(){
+        var form_name = $("#form_name").val();
+
+        localStorage.setItem("form_name", form_name);
+
+   });
+
+   var form_name = localStorage.getItem("form_name");
+
+   if(form_name != null && form_name != "underfined"){
+        var about_parrafo = $("#about p");
+        about_parrafo.html("<br><strong>Bienvenido, " + form_name + "</strong>");
+        about_parrafo.append("<a href='#' id='logout'>Cerrar sesión</a>");
+        $("#login").hide();
+
+        $("#logout").click(function(){
+            localStorage.clear();
+            location.reload();
+        });
+   }
+
 });
